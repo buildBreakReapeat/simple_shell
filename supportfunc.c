2600d05f003d98ff5ca4e71f3d36ac3a10ab2b21
jay-alx <126336909+buildBreakReapeat@users.noreply.github.com>
2023-05-22 07:58:26 +0000

    update supportfunc.c

--- supportfunc.c
+++ supportfunc.c
@@ -29,3 +29,62 @@ int is_str(char *str)
 	return (result);
 }
 
+/**
+ * is_setenv - checker for setenv and edge cases
+ * @str: String to compare
+ *
+ * Return: If there's a coincidence or not
+ */
+int is_setenv(char *str)
+{
+	char *clone = _strdup(str), *name = NULL, *value = NULL;
+
+	if (strcmp(_strtok(clone, ' '), "setenv") == 0)
+	{
+		name = _strtok(NULL, ' ');
+		value = _strtok(NULL, ' ');
+		if (name == NULL || value == NULL)
+		{
+			write(STDERR_FILENO, "Usage: setenv [VARIABLE] [VALUE]\n", 34);
+			free(clone);
+			return (1);
+		}
+		_setenv(name, value, 1);
+		free(clone);
+		return (1);
+	}
+	free(clone);
+	return (0);
+}
+
+/**
+ * is_unset - checker for setenv and edge cases
+ * @str: String to compare
+ *
+ * Return: If there's a coincidence or not
+ */
+int is_unset(char *str)
+{
+	char *clone = _strdup(str), *token = NULL;
+
+	if (strcmp(_strtok(clone, ' '), "unsetenv") == 0)
+	{
+		token = _strtok(NULL, ' ');
+		if (token == NULL)
+		{
+			write(STDERR_FILENO, "Usage: unsetenv [VARIABLE]\n", 28);
+			free(clone);
+			return (1);
+		}
+		if (_unsetenv(token) == -1)
+		{
+			write(STDERR_FILENO, "can't unset non-existent variable\n", 35);
+			free(clone);
+			return (1);
+		}
+		free(clone);
+		return (1);
+	}
+	free(clone);
+	return (0);
+}
