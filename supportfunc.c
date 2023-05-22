18a9651bd2a8e15e71e96a46ca67bf5904cd99de
jay-alx <126336909+buildBreakReapeat@users.noreply.github.com>
2023-05-22 08:02:46 +0000

    update supportfunc.c

--- supportfunc.c
+++ supportfunc.c
@@ -127,3 +127,26 @@ int cd_handler(char *str)
 	free(PWD);
 	return (builtIn);
 }
+
+/**
+ * _checkBuiltIn - Checks if there's a Built-In in the buffer or not
+ * @str: String to traverse
+ *
+ * Return: If there's a coincidence or not
+ */
+int _checkBuiltIn(char *str)
+{
+	if (checkDir(str) == 1)
+		return (2);
+	if (checkExit(str) == 1)
+		return (1);
+	if (checkEnv(str) == 1)
+		return (3);
+	if (checkUnset(str) == 1)
+		return (4);
+	if (checkSetenv(str) == 1)
+		return (5);
+	if (checkHelp(str) == 1)
+		return (6);
+	return (0);
+}
