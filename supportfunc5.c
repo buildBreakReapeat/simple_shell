7b0a9d8ff8f31b58432687954796c5c35242b0bf
jay-alx <126336909+buildBreakReapeat@users.noreply.github.com>
2023-05-22 08:54:06 +0000

    update supportfunc5.c

--- supportfunc5.c
+++ supportfunc5.c
@@ -32,3 +32,32 @@ char *_strcat(char *s1, char *s2)
 		str[n + i - 1] = s2[n];
 	return (str);
 }
+
+/**
+ * _strcmp - Strcmp function
+ * @s1: String 1
+ * @s2: String 2
+ *
+ * Return: Returns the difference of the strings
+ */
+int _strcmp(const char *s1, const char *s2)
+{
+	int i, ss1 = 0, ss2 = 0, result = 0;
+
+	for (i = 0; i > -1; i++)
+	{
+		if (s1[i] != s2[i])
+		{
+			ss1 = s1[i];
+			ss2 = s2[i];
+			break;
+		}
+		else
+		{
+			if (s1[i] == '\0' || s2[i] == '\0')
+				break;
+		}
+	}
+	result = ss1 - ss2;
+	return (result);
+}
