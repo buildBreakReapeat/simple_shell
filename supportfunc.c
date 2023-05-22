23723e1ba1d2146b313e3d1fe14f28d18b2e72c6
jay-alx <126336909+buildBreakReapeat@users.noreply.github.com>
2023-05-22 08:01:23 +0000

    update supportfunc.c

--- supportfunc.c
+++ supportfunc.c
@@ -88,3 +88,42 @@ int is_unset(char *str)
 	free(clone);
 	return (0);
 }
+
+/**
+ * cd_handler - checker for cd (cd function)
+ * @str: String to compare
+ *
+ * Return: If there's a coincidence or not
+ */
+int cd_handler(char *str)
+{
+	char *cpy = _strdup(str), *dir = NULL, *PWD = NULL, *buff = NULL;
+	int builtIn = 0, flag = 0;
+
+	if (_strcmp(_strtok(cpy, ' '), "cd") == 0)
+	{
+		dir = _strtok(NULL, ' ');
+		if (dir == NULL)
+		{
+			dir = _getenv("HOME");
+			flag++;
+		}
+		if (_strcmp(dir, "-") == 0)
+		{
+			dir = _getenv("OLDPWD");
+			flag++;
+		}
+		PWD = _getenv("PWD");
+		_setenv("OLDPWD", PWD, 1);
+		_setenv("PWD", dir, 1);
+		if (chdir(dir) != 0)
+			perror(dir);
+		builtIn++;
+	}
+	if (flag > 0)
+		free(dir);
+	free(cpy);
+	free(buff);
+	free(PWD);
+	return (builtIn);
+}
