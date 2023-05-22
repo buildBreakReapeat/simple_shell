088bb6ed1ba8c492c3a7b8239a230818c514732f
jay-alx <126336909+buildBreakReapeat@users.noreply.github.com>
2023-05-22 07:34:52 +0000

    update main.h

--- main.h
+++ main.h
@@ -25,15 +25,24 @@ typedef struct list
 } list_t;
 
 char *_strdup(const char *str);
-char *_strcat(char *s1, char *s2);
-int _strcmp(const char *s1, const char *s2);
-int _strlen(const char *s);
+char *_strcat(char *str1, char *str2);
+int _strcmp(const char *str1, const char *str2);
+int _strlen(const char *str);
 char *_strtok(char *str, char delim);
 char *_strcpy(char *dest, char *src);
 char *_getenv(const char *name);
 void signal_isatty(void);
 ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
 
-
-
+void printenv(void);
+int is_str(char *str);
+char *path(char *filename);
+int _setenv(char *name, char *value, int overwrite);
+int is_unset(char *str);
+int is_setenv(char *str);
+int is_help(char *str);
+int _unsetenv(char *name);
+void ctrl_c_handler(int signum);
+int _atoi(char *s);
+int is_dir(char *str);
 #endif
