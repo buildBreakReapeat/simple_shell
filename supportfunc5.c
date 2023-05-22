858aeecfbf45d9cdce4a1740a96b2d2987b174ea
jay-alx <126336909+buildBreakReapeat@users.noreply.github.com>
2023-05-22 08:53:46 +0000

    support5 this will never end

---
+++ supportfunc5.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _strcat - Concatenates two strings
+ * @s1: First string
+ * @s2: Second string
+ *
+ * Return: Returns the concatenated string
+ */
+char *_strcat(char *s1, char *s2)
+{
+	int i = 0, j = 0, n = 0;
+	char *str;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	do {
+		i++;
+	} while (s1[i - 1]);
+	do {
+		j++;
+	} while (s2[j - 1]);
+	str = malloc(sizeof(char) * (i + j - 1));
+	if (str == NULL)
+		return (NULL);
+
+	for (n = 0; n < i; n++)
+		str[n] = s1[n];
+	for (n = 0; n < j; n++)
+		str[n + i - 1] = s2[n];
+	return (str);
+}
