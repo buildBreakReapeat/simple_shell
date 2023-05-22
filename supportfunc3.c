3be899da9a58130ec8ef568babfaea01200dc075
jay-alx <126336909+buildBreakReapeat@users.noreply.github.com>
2023-05-22 08:46:32 +0000

    update supportfunc3.c

--- supportfunc3.c
+++ supportfunc3.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * trim_buff - Removes the '\n' char, and looks for tabulations
+ * trim_buff - Removes the '\t' char, '\n'
  * @str: The buffer
- * @counter: Length of the string
+ * @size: size string
  *
  * Return: The clean buffer
  */
-char *trim_buff(char *str, int counter)
+char *trim_buff(char *str, int size)
 {
 	int i = 0, tab = 0;
-	str[counter - 1] = '\0';
+	str[size - 1] = '\0';
 	
 
 	while (str[i])
@@ -33,6 +33,122 @@ char *trim_buff(char *str, int counter)
 			i++;
 		}
 	}
-	str = comments(str);
+	str = trim_hash(str);
 	return (str);
+}
+
+
+/**
+ * trim_hash - Checks for comments
+ * @str: String to traverse
+ *
+ * Return: clean str if comments, if not same str
+ */
+char *trim_hash(char *str)
+{
+	int i = 0, j = 0, hash = 0, flag = 0;
+
+	while (str[j])
+	{
+		if (str[j] == ' ' && str[j + 1] == '#')
+		{
+			hash++;
+			break;
+		}
+		j++;
+	}
+	if (hash == 0)
+		return (str);
+	while (str[i])
+	{
+		if (flag == 1)
+		{
+			str[i] = ' ';
+			i++;
+			continue;
+		}
+		if (str[i] == ' ' && str[i + 1] == '#')
+		{
+			flag++;
+			i++;
+			continue;
+		}
+		i++;
+	}
+	return (str);
+}
+
+
+/**
+ * itsExecutable - Checks if path i executable
+ * @path: path of the file
+ *
+ * Return: If executable or not
+ */
+int itsExecutable(char *path)
+{
+	if (isDir(path) == 0)
+	{
+		if (path[0] == '/')
+			perror(path);
+		else
+			perror(path);
+		return (1);
+	}
+	if (access(path, X_OK))
+		return (2);
+	else
+		return (0);
+}
+
+
+
+/**
+ * isDir - Checks if the path is a directory
+ * @path: path of the file
+ *
+ * Return: If dir or not
+ */
+int isDir(const char *path)
+{
+	struct stat st = {0};
+
+	stat(path, &st);
+	return (S_ISREG(st.st_mode));
+}
+
+
+/**
+ * getReturnValue - Get exit value
+ * @str: String to traverse
+ *
+ * Return: The exit value
+ */
+int getReturnValue(char *str)
+{
+	char *cpy = _strdup(str), *token;
+	int exitValue = 0;
+
+	token = _strtok(cpy, ' ');
+	token = _strtok(NULL, ' ');
+	if (token == NULL)
+	{
+		free(cpy);
+		return (0);
+	}
+	if (_isdigit(token) == 0)
+		exitValue = _atoi(token);
+	else
+	{
+		free(cpy);
+		write(STDERR_FILENO, "exit: Illegal number\n", 22);
+		return (-1);
+	}
+	free(cpy);
+	if (exitValue < 0)
+	{
+		write(STDERR_FILENO, "exit: Illegal number\n", 22);
+		exitValue = (-1);
+	}
+	return (exitValue);
 }
