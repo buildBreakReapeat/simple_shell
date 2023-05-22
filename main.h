df3f8f96d06a9e6b5861b91a89997e1137f51f73
jay-alx <126336909+buildBreakReapeat@users.noreply.github.com>
2023-05-22 07:47:55 +0000

    update 1 file and create 1 file

--- main.h
+++ main.h
@@ -52,7 +52,7 @@ int _isdigit(char *str);
 int is_exit(char *str);
 char *create_variable(char *name, char *value);
 char *trim_hash(char *str);
-int is_dir(char *str);
+int cd_handler(char *str);
 char *trim_buff(char *str, int counter);
 int checkEnv(char *str);
 void case_assist(char *name);
